2b1c9dc0c1f25eaf0e8f157eef5262e09c371bf0
QosimovRahmonjonn <qosimovrahmonjon2023@gmail.com>
2024-02-12 12:00:07 +0500

    header added

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const btnMenu = document.getElementById("btn-menu");
+
+btnMenu.addEventListener("click", function () {
+  document.getElementById("nav-items").classList.toggle("-translate-x-full");
+});
